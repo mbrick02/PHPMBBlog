e15de84a8ff24113a536d77f226f2108a7c1eb27
mbrick02 <mbrick02@gmail.com>
2018-09-17 15:46:28 -0400

    add returnText ver method for template class

--- private/classes/template.class.php
+++ private/classes/template.class.php
@@ -8,6 +8,7 @@ class Template {
 		$this->assignedVars[$key] = $value;
 	}
 
+
   function display($filename, $assignedVars) {
   	if(file_exists($this->$filename)) {
   		$output = file_get_contents($this->$filename);
@@ -20,4 +21,26 @@ class Template {
   		echo "*** Missing template error ****";
   	}
   }
+
+  function returnText($filename, $assignedVars) {
+  	if(file_exists($this->$filename)) {
+  		$output = file_get_contents($this->$filename);
+  		foreach($this->$assignedVars as $key => $value) {
+  			$output = pregReplace('/{'.$key.'}/', $value, $output);
+  			// above is: regular expression replace
+  		}
+  		return $output;
+  	} else {
+  		return "*** Missing template error {$filename} ****";
+  	}
+  }
 }
+
+/* usage:
+$template = new Template();
+$template->filename = "template2.php";
+$template->set('pageTitle', "Template Test");
+$template->set('content', "This is a test of templating using search replace.");
+$template->display();
+*/
+?>

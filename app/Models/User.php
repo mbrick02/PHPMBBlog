51e355c96f6719c84855135c876365fafae974f0
mbrick02 <mbrick02@gmail.com>
2018-10-09 18:00:16 -0400

    User ideas

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace app\Models;
-// use private\classes\db.class.php;
-// PRIVATE_PATH . DS . 'classes' . DS . 'db.class.php';
 
 class User extends DB {
-
+// id, username, email, fname, lname, password,
+//    remember_token, created_at, updated_at
+/*
+remember_token
+No. It's not supposed to be used to authenticate.
+It's used by the framework to help against Remember Me cookie hijacking.
+The value is refreshed upon login and logout.
+If a cookie is hijacked by a malicious person,
+logging out makes the hijacked cookie useless since it doesn't match anymore.
+*/
 
 }
